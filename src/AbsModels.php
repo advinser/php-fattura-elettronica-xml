e4f6a31ef33b4d8883579a284f12271647309887
gabrielandrei <gabrielandrei79@gmail.com>
2019-01-14 10:46:24 +0100

    fix AbsModels for allow empty 'ContattiTrasmittente'

--- src/AbsModels.php
+++ src/AbsModels.php
@@ -19,9 +19,9 @@ abstract class AbsModels
     {
         foreach ($array as $key => $item) {
             is_array($item) && $array [$key] = $this->clean_array($item);
-            if ( $key!=='ContattiTrasmittente' && empty ($array [$key])){
-                unset ($array [$key]);
-            }
+//            if ( $key!=='ContattiTrasmittente' && empty ($array [$key])){
+//                unset ($array [$key]);
+//            }
         }
         return $array;
     }
